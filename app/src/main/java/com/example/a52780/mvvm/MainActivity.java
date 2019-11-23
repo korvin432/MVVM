d401409185d3b67d414d00e5a90079daf62039e5
Roman Kr <roma_as@list.ru>
2019-11-23 16:44:36 +0300

    ENTITY

---
+++ app/src/main/java/com/example/a52780/mvvm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a52780.mvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
