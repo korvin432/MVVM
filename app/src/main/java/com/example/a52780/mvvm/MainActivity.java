328995ecd9101d61317400a529dadb201afdb67f
Roman Kr <roma_as@list.ru>
2019-11-24 12:48:58 +0300

    VIEWMODEL

--- app/src/main/java/com/example/a52780/mvvm/MainActivity.java
+++ app/src/main/java/com/example/a52780/mvvm/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.a52780.mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update recyclerview
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
